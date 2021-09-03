520be954da1a2b634458a8e28d9649897e797573
gogobody <138950128@qq.com>
2021-09-03 20:00:34 +0800

    更新 php 8.0 支持

--- admin/header.php
+++ admin/header.php
@@ -3,28 +3,24 @@ if (!defined('__TYPECHO_ADMIN__')) {
     exit;
 }
 
-$header = '<link rel="stylesheet" href="' . Typecho_Common::url('normalize.css?v=' . $suffixVersion, $options->adminStaticUrl('css')) . '">
-<link rel="stylesheet" href="' . Typecho_Common::url('grid.css?v=' . $suffixVersion, $options->adminStaticUrl('css')) . '">
-<link rel="stylesheet" href="' . Typecho_Common::url('style.css?v=' . $suffixVersion, $options->adminStaticUrl('css')) . '">
-<!--[if lt IE 9]>
-<script src="' . Typecho_Common::url('html5shiv.js?v=' . $suffixVersion, $options->adminStaticUrl('js')) . '"></script>
-<script src="' . Typecho_Common::url('respond.js?v=' . $suffixVersion, $options->adminStaticUrl('js')) . '"></script>
-<![endif]-->';
+$header = '<link rel="stylesheet" href="' . $options->adminStaticUrl('css', 'normalize.css', true) . '">
+<link rel="stylesheet" href="' . $options->adminStaticUrl('css', 'grid.css', true) . '">
+<link rel="stylesheet" href="' . $options->adminStaticUrl('css', 'style.css', true) . '">';
 
 /** 注册一个初始化插件 */
 $header = Typecho_Plugin::factory('admin/header.php')->header($header);
+
 ?><!DOCTYPE HTML>
-<html class="no-js" data-color-mode="<?php if($_COOKIE['night']=='1')echo 'dark';else echo 'light'; ?>">
-    <head>
-        <meta charset="<?php $options->charset(); ?>">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="renderer" content="webkit">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title><?php _e('%s - %s - Powered by Typecho', $menu->title, $options->title); ?></title>
-        <meta name="robots" content="noindex, nofollow">
-        <?php echo $header; ?>
-    </head>
-    <body<?php if (isset($bodyClass)) {echo ' class="' . $bodyClass . '"';} ?>>
-    <!--[if lt IE 9]>
+<html data-color-mode="<?php if($_COOKIE['night']=='1')echo 'dark';else echo 'light'; ?>">
+<head>
+    <meta charset="<?php $options->charset(); ?>">
+    <meta name="renderer" content="webkit">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title><?php _e('%s - %s - Powered by Typecho', $menu->title, $options->title); ?></title>
+    <meta name="robots" content="noindex, nofollow">
+    <?php echo $header; ?>
+</head>
+<body<?php if (isset($bodyClass)) {echo ' class="' . $bodyClass . '"';} ?>>
+<!--[if lt IE 9]>
         <div class="message error browsehappy" role="dialog"><?php _e('当前网页 <strong>不支持</strong> 你正在使用的浏览器. 为了正常的访问, 请 <a href="http://browsehappy.com/">升级你的浏览器</a>'); ?>.</div>
     <![endif]-->
